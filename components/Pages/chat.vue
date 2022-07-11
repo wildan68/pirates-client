6d566c50304c413ee3e58a83f3e69a7ea695d54e
Mohammad Wildan Rizkiyadi <outsiderkemuning@gmail.com>
2022-07-11 18:52:12 +0700

    chat

--- components/Pages/chat.vue
+++ components/Pages/chat.vue
@@ -1,13 +1,57 @@
 <template>
 <div class="flex flex-col gap-[20px]">
-    <div class="w-full bg-white border-box border-[4px] gap-[10px] py-[30px] px-[5em] rounded-lg flex flex-col items-center">
-        TEST CHAT
+    <div class="w-full bg-white border-box border-[4px] gap-[10px] py-[30px] px-[10px] rounded-lg flex flex-col items-center">
+        <div class="h-[450px] w-full flex-col flex justify-end overflow-hidden">
+            <div class="flex gap-[5px]" v-for="(chat, i) in $server.msg.chat" :key="i">
+                <span class="text-black">{{ chat.nick }}</span>
+                <span class="text-black">:</span>
+                <span class="text-black">{{ chat.text }}</span>
+            </div>
+        </div>
+    </div>
+    <div class="w-full bg-white border-box border-[4px] gap-[10px] py-[20px] px-[10px] rounded-lg flex items-center">
+        <input type="text" class="w-full" placeholder="Ketik disini.." v-model="textChat"/>
+        <button class="btn btn-common text-white" style="padding-left: 15px; padding-right: 15px" @click.prevent="sendChat">
+            <i class="bi bi-chat-dots-fill"></i>
+        </button>
     </div>
 </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            textChat: '',
+            btnChat: true,
+        }
+    },
+    methods: {
+        sendChat() {
+            if (!this.btnChat) {
+                this.$server.host.send(JSON.stringify({
+                // cmd 1005 = custom notif
+                cmd: 1005,
+                data: {
+                    type: 1,
+                    text: "Tunggu 10 Detik untuk mengirim kembali",
+                }
+                }))
+                return;
+            } 
+            this.$server.host.send(JSON.stringify({
+                // cmd 1004 = send chat
+                cmd: 1004,
+                data: {
+                    text: this.textChat,
+                }
+            }));
+            this.textChat = '';
+            this.btnChat = false;
+            setTimeout(() => {
+                this.btnChat = true;
+            }, 10000);
+        },
+    }
 }
 </script>
